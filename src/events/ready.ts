c93eadb7d94f64d13862880b33173bd1e89e021e
Squiddleton <mdlegomaster@gmail.com>
2022-07-27 11:45:41 -0400

    Exclude non-members from winning giveaways

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,4 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Message } from 'discord.js';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelType, EmbedBuilder, Message, Snowflake } from 'discord.js';
 import { schedule } from 'node-cron';
 import client from '../clients/discord.js';
 import { Event } from '../types/types.js';
@@ -45,54 +45,58 @@ export default new Event({
 
 			for (const giveaway of giveaways) {
 				const giveawayChannel = validateChannel(client, giveaway.channelId, 'Giveaway channel');
+				if (giveawayChannel.type !== ChannelType.DM) {
+					let message: Message;
+					try {
+						message = await giveawayChannel.messages.fetch(giveaway.messageId);
+					}
+					catch {
+						console.error('The message for the following giveaway no longer exists:', giveaway);
+						return;
+					}
 
-				let message: Message;
-				try {
-					message = await giveawayChannel.messages.fetch(giveaway.messageId);
+					const winnerIds: Snowflake[] = [];
+					const entrantsInGuild = await giveawayChannel.guild.members.fetch({ user: giveaway.entrants });
+					const entrantIds = entrantsInGuild.map(member => member.id);
+
+					for (let i = 0; i < giveaway.winnerNumber && i < entrantIds.length; i++) {
+						const winnerId = randomFromArray(entrantIds);
+						if (!winnerIds.includes(winnerId)) winnerIds.push(winnerId);
+					}
+					const winnersDisplay = winnerIds.map((w, i) => `${i + 1}. <@${w}> (${w})`).join('\n');
+
+					const row = new ActionRowBuilder<ButtonBuilder>({ components: [
+						new ButtonBuilder()
+							.setLabel('Enter')
+							.setCustomId('giveaway')
+							.setStyle(ButtonStyle.Success)
+							.setDisabled(true)
+					] });
+					await message.edit({
+						embeds: [
+							new EmbedBuilder(message.embeds[0].toJSON())
+								.setColor('Red')
+								.setFields([
+									{ name: 'Winners', value: winnersDisplay === '' ? 'None' : winnersDisplay.slice(0, 1024), inline: true },
+									{ name: 'Time', value: `Started <t:${giveaway.startTime}:R>\nEnded <t:${giveaway.endTime}:R>`, inline: true }
+								])
+						],
+						components: [row]
+					});
+
+					giveaway.completed = true;
+					giveaway.winners = winnerIds;
+					await guildSchema.updateOne(
+						{
+							_id: message.guildId,
+							'giveaways.messageId': giveaway.messageId
+						},
+						{ $set: { 'giveaways.$': giveaway } }
+					);
+
+					if (winnerIds.length === 0) return message.reply('This giveaway has concluded!  Unfortunately, no one entered . . .');
+					return message.reply(`This giveaway has concluded!  Congratulations to the following winners:\n${winnersDisplay}\nIf you won, please ensure that you have enabled DMs within the server in order to receive your prize.`);
 				}
-				catch {
-					console.error('The message for the following giveaway no longer exists:', giveaway);
-					return;
-				}
-
-				const winnerIds: string[] = [];
-				for (let i = 0; i < giveaway.winnerNumber && i < giveaway.entrants.length; i++) {
-					const entrants = giveaway.entrants.filter(e => !winnerIds.includes(e));
-					if (entrants.length) winnerIds.push(randomFromArray(entrants));
-				}
-				const winnersDisplay = winnerIds.map((w, i) => `${i + 1}. <@${w}> (${w})`).join('\n');
-
-				const row = new ActionRowBuilder<ButtonBuilder>({ components: [
-					new ButtonBuilder()
-						.setLabel('Enter')
-						.setCustomId('giveaway')
-						.setStyle(ButtonStyle.Success)
-						.setDisabled(true)
-				] });
-				await message.edit({
-					embeds: [
-						new EmbedBuilder(message.embeds[0].toJSON())
-							.setColor('Red')
-							.setFields([
-								{ name: 'Winners', value: winnersDisplay.slice(0, 1024) || 'None', inline: true },
-								{ name: 'Time', value: `Started <t:${giveaway.startTime}:R>\nEnded <t:${giveaway.endTime}:R>`, inline: true }
-							])
-					],
-					components: [row]
-				});
-
-				giveaway.completed = true;
-				giveaway.winners = winnerIds;
-				await guildSchema.updateOne(
-					{
-						_id: message.guildId,
-						'giveaways.messageId': giveaway.messageId
-					},
-					{ $set: { 'giveaways.$': giveaway } }
-				);
-
-				if (winnerIds.length === 0) return message.reply('This giveaway has concluded!  Unfortunately, no one entered . . .');
-				return message.reply(`This giveaway has concluded!  Congratulations to the following winners:\n${winnersDisplay}\nIf you won, please ensure that you have enabled DMs within the server in order to receive your prize.`);
 			}
 		});
 	}
