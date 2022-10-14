b24e52c4c8fc549d339bdba4fab11227cad11e33
Squiddleton <mdlegomaster@gmail.com>
2022-10-14 11:44:41 -0400

    Handle Reddit in snoowrap file

--- src/clients/snoowrap.ts
+++ src/clients/snoowrap.ts
@@ -1,5 +1,32 @@
+import { CommentStream, SubmissionStream } from 'snoostorm';
 import RedditClient from 'snoowrap';
 import config from '../config.js';
+import type { DiscordClient } from './discord.js';
 
 const reddit = new RedditClient(config.snoowrap);
 export default reddit;
+
+export const handleReddit = (client: DiscordClient) => {
+	const fnbrSubmissions = new SubmissionStream(reddit, {
+		subreddit: 'FortniteBR',
+		pollTime: 15000
+	});
+	const fnbrComments = new CommentStream(reddit, {
+		subreddit: 'FortniteBR',
+		pollTime: 40000
+	});
+	setTimeout(() => {
+		fnbrSubmissions.on('item', async submission => {
+			if (submission.author.name === 'TheFortniteTeam' || submission.author_flair_text?.includes('Epic Games')) {
+				await client.devChannel.send(`New Epic post detected: https://www.reddit.com${submission.permalink}`);
+			}
+		});
+	}, 20000);
+	setTimeout(() => {
+		fnbrComments.on('item', async comment => {
+			if (comment.author.name === 'TheFortniteTeam' || comment.author_flair_text?.includes('Epic Games')) {
+				await client.devChannel.send(`New Epic comment detected: https://www.reddit.com${comment.permalink}`);
+			}
+		});
+	}, 45000);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
 import mongoose from 'mongoose';
-import { CommentStream, SubmissionStream } from 'snoostorm';
 import config from './config.js';
 import client from './clients/discord.js';
-import snoowrap from './clients/snoowrap.js';
+import { handleReddit } from './clients/snoowrap.js';
 
 client.login(config.token);
 mongoose.connect(config.mongoPath);
-
-const fnbrSubmissions = new SubmissionStream(snoowrap, {
-	subreddit: 'FortniteBR',
-	pollTime: 15000
-});
-const fnbrComments = new CommentStream(snoowrap, {
-	subreddit: 'FortniteBR',
-	pollTime: 40000
-});
-setTimeout(() => {
-	fnbrSubmissions.on('item', async submission => {
-		if (submission.author.name === 'TheFortniteTeam' || submission.author_flair_text?.includes('Epic Games')) {
-			await client.devChannel.send(`New Epic post detected: https://www.reddit.com${submission.permalink}`);
-		}
-	});
-}, 20000);
-setTimeout(() => {
-	fnbrComments.on('item', async comment => {
-		if (comment.author.name === 'TheFortniteTeam' || comment.author_flair_text?.includes('Epic Games')) {
-			await client.devChannel.send(`New Epic comment detected: https://www.reddit.com${comment.permalink}`);
-		}
-	});
-}, 45000);
+handleReddit(client);
