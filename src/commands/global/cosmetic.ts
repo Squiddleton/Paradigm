a106c3b49aec6fa12d88f26a4d66570424e9b113
Squiddleton <mdlegomaster@gmail.com>
2022-10-19 16:54:09 -0400

    Add style listeners to /cosmetic

--- src/commands/global/cosmetic.ts
+++ src/commands/global/cosmetic.ts
@@ -1,9 +1,10 @@
-import { ApplicationCommandOptionType } from 'discord.js';
+import { ActionRowBuilder, ApplicationCommandOptionType, ButtonBuilder, ButtonStyle, MessageActionRowComponentBuilder, SelectMenuBuilder } from 'discord.js';
 import type { Language } from '@squiddleton/fortnite-api';
 import { SlashCommand } from '@squiddleton/discordjs-util';
 import { createCosmeticEmbed, findCosmetic } from '../../util/fortnite.js';
-import { LanguageChoices } from '../../util/constants.js';
+import { ErrorMessage, LanguageChoices } from '../../util/constants.js';
 import fortniteAPI from '../../clients/fortnite.js';
+import { messageComponentCollectorFilter } from '../../util/functions.js';
 
 export default new SlashCommand({
 	name: 'cosmetic',
@@ -27,14 +28,89 @@ export default new SlashCommand({
 	async execute(interaction) {
 		await interaction.deferReply();
 
-		const item = await findCosmetic(interaction.options.getString('cosmetic', true));
+		const cosmetic = await findCosmetic(interaction.options.getString('cosmetic', true));
 		const language = interaction.options.getString('language') as Language | null;
 
-		if (item === null) {
+		if (cosmetic === null) {
 			await interaction.editReply('No cosmetic matches your query.');
+			return;
 		}
-		else {
-			await interaction.editReply({ embeds: [createCosmeticEmbed(language === null ? item : await fortniteAPI.findCosmetic({ id: item.id, language }))] });
+
+		const { variants } = cosmetic;
+		const embed = createCosmeticEmbed(language === null ? cosmetic : await fortniteAPI.findCosmetic({ id: cosmetic.id, language }));
+
+		if (!variants?.length) {
+			await interaction.editReply({ embeds: [embed] });
+			return;
 		}
+
+		const components = [
+			new ActionRowBuilder<ButtonBuilder>()
+				.setComponents(
+					new ButtonBuilder()
+						.setCustomId('featured')
+						.setLabel('Featured Image')
+						.setStyle(ButtonStyle.Primary),
+					new ButtonBuilder()
+						.setCustomId('lock')
+						.setLabel('Lock Image In')
+						.setStyle(ButtonStyle.Danger)
+				),
+			...variants.slice(0, 4).map(variant => new ActionRowBuilder<MessageActionRowComponentBuilder>()
+				.setComponents(new SelectMenuBuilder()
+					.setCustomId(variant.channel)
+					.setMinValues(1)
+					.setPlaceholder(variant.type.toUpperCase())
+					.setOptions(variant.options.slice(0, 25).map(option => ({ label: option.name.toUpperCase(), value: option.tag })))
+				)
+			)];
+
+		const message = await interaction.editReply({ components, embeds: [embed] });
+		const collector = message.createMessageComponentCollector({ filter: messageComponentCollectorFilter(interaction), time: 120000 });
+
+		collector
+			.on('collect', async i => {
+				if (i.isButton()) {
+					switch (i.customId) {
+						case 'featured': {
+							for (const component of components) {
+								component.setComponents(component.components.map(menu => {
+									return menu instanceof SelectMenuBuilder
+										? menu.setOptions(menu.options.map(option => ({ label: option.data.label ?? 'label', value: option.data.value ?? 'value' })))
+										: menu;
+								}
+								));
+							}
+							await i.update({ components, embeds: [embed.setImage(cosmetic.images.featured ?? cosmetic.images.icon)] });
+							break;
+						}
+						case 'lock': {
+							await i.update({ components: [] });
+							break;
+						}
+					}
+					return;
+				}
+
+				const variantChosen = variants.find(v => v.channel === i.customId);
+				if (variantChosen === undefined) throw new Error(ErrorMessage.UnexpectedValue.replace('{value}', `${variantChosen}`));
+
+				const optionChosen = variantChosen.options.find(o => o.tag === i.values[0]);
+				if (optionChosen === undefined) throw new Error(ErrorMessage.UnexpectedValue.replace('{value}', `${optionChosen}`));
+
+				for (const component of components) {
+					component.setComponents(component.components.map(menu => {
+						return menu instanceof SelectMenuBuilder
+							? menu.setOptions(menu.options.map(o => ({ label: o.data.label ?? 'label', value: o.data.value ?? 'value', default: menu.data.custom_id === i.customId && o.data.value === optionChosen.tag })))
+							: menu;
+					}
+					));
+				}
+
+				await i.update({ components, embeds: [embed.setImage(optionChosen.image)] });
+			})
+			.on('end', async () => {
+				await interaction.editReply({ components: [] });
+			});
 	}
 });
