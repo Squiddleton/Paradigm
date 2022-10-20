3f40576191010140d54f50e25dcbd4498cef279e
Squiddleton <mdlegomaster@gmail.com>
2022-10-20 17:52:35 -0400

    Standardize typeguard paramater names

--- src/util/typeguards.ts
+++ src/util/typeguards.ts
@@ -2,6 +2,6 @@ import { BackgroundURL, RarityOrdering, UnitsToMS } from './constants';
 
 export const isBackground = (str: string): str is keyof typeof BackgroundURL => str in BackgroundURL;
 
-export const isRarity = (rarity: string): rarity is keyof typeof RarityOrdering => rarity in RarityOrdering;
+export const isRarity = (str: string): str is keyof typeof RarityOrdering => str in RarityOrdering;
 
-export const isUnit = (unit: string): unit is keyof typeof UnitsToMS => unit in UnitsToMS;
+export const isUnit = (str: string): str is keyof typeof UnitsToMS => str in UnitsToMS;
