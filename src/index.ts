8ea6f8f326d1617da332331cc0aa58274b8ff383
Squiddleton <mdlegomaster@gmail.com>
2022-12-07 11:43:32 -0500

    Throw errors for unexpect mongoose properties

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { connect } from 'mongoose';
+import { connect, set } from 'mongoose';
 import client from './clients/discord.js';
 import { handleReddit } from './clients/snoowrap.js';
 import { handleTwitter } from './clients/twitter.js';
 import config from './config.js';
 
 client.login(config.token);
+set('strictQuery', 'throw');
 connect(config.mongoPath);
 handleReddit(client);
 handleTwitter(client);
