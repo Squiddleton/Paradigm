dd213779a103e1c7e008d01f3c2ae9c009929e40
Squiddleton <mothmain9515@outlook.com>
2023-05-18 08:46:22 -0400

    Handle imgur 429 errors

--- src/commands/global/boris.ts
+++ src/commands/global/boris.ts
@@ -8,11 +8,17 @@ export default new SlashCommand({
 	description: 'Send an image of the goodest boy',
 	scope: 'Global',
 	async execute(interaction) {
-		const data: string[] = (await Promise.all(BorisAlbumIds.map(a => imgurClient.Album.get(a))))
-			.map(a => a.data.images)
-			.flat()
-			.map(i => i.link);
+		try {
+			const data: string[] = (await Promise.all(BorisAlbumIds.map(a => imgurClient.Album.get(a))))
+				.map(a => a.data.images)
+				.flat()
+				.map(i => i.link);
 
-		await interaction.reply({ files: [getRandomItem(data)] });
+			await interaction.reply({ files: [getRandomItem(data)] });
+		}
+		catch (e) {
+			if (typeof e === 'object' && !(e instanceof Error) && e !== null && 'status' in e && e.status === 429) await interaction.reply({ content: 'Imgur is temporarily over capacity for fetching images; please try again later.', ephemeral: true });
+			else throw e;
+		}
 	}
 });
