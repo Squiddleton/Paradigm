bc4f2864960ceb20759f2193e010dc16352e4748
Squiddleton <mothmain9515@outlook.com>
2023-07-03 23:49:19 -0400

    Fix to avoid resetting refresh token

--- src/clients/epic.ts
+++ src/clients/epic.ts
@@ -1,7 +1,13 @@
 import { EpicClient } from '@squiddleton/epic';
 import config from '../config.js';
 
-const epicClient = new EpicClient({ autoRefresh: true });
-epicClient.auth.authenticate(config.epicDeviceAuth.device1);
+const epicClient = new EpicClient();
 
 export default epicClient;
+
+const authenticate = async () => {
+	await epicClient.auth.authenticate(config.epicDeviceAuth.device1);
+	setTimeout(authenticate, 7200000);
+};
+
+await authenticate();
