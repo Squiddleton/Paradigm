31a111d0912dbbb75814b2de924de312a76b5e6d
Squiddleton <mothmain9515@outlook.com>
2023-07-03 23:50:36 -0400

    Authenticate Epic client in index

--- src/clients/epic.ts
+++ src/clients/epic.ts
@@ -1,13 +1,5 @@
 import { EpicClient } from '@squiddleton/epic';
-import config from '../config.js';
 
 const epicClient = new EpicClient();
 
 export default epicClient;
-
-const authenticate = async () => {
-	await epicClient.auth.authenticate(config.epicDeviceAuth.device1);
-	setTimeout(authenticate, 7200000);
-};
-
-await authenticate();
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { set } from 'mongoose';
 import client from './clients/discord.js';
+import epicClient from './clients/epic.js';
 import config from './config.js';
 import { handleDisconnect } from './util/functions.js';
 
 await set('strictQuery', 'throw')
 	.connect(config.mongoPath)
 	.catch(handleDisconnect);
+
+const authenticate = async () => {
+	await epicClient.auth.authenticate(config.epicDeviceAuth.device1);
+	setTimeout(authenticate, 7200000);
+};
+await authenticate();
 
 await client
 	.login(config.token)
