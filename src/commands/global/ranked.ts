a5a007b8b0418dd71272a30f0d8ba3998770566f
Squiddleton <mothmain9515@outlook.com>
2023-07-05 16:46:55 -0400

    Add /ranked

---
+++ src/commands/global/ranked.ts
@@ -0,0 +1,62 @@
+import { SlashCommand } from '@squiddleton/discordjs-util';
+import type { AccountType, Stats } from '@squiddleton/fortnite-api';
+import { ApplicationCommandOptionType } from 'discord.js';
+import fortniteAPI from '../../clients/fortnite.js';
+import { PlatformChoices } from '../../util/constants.js';
+import { createRankedImage, handleStatsError, linkEpicAccount } from '../../util/fortnite.js';
+import { getUser } from '../../util/users.js';
+
+export default new SlashCommand({
+	name: 'ranked',
+	description: 'Display a Fortnite player\'s Ranked stats',
+	options: [
+		{
+			name: 'player',
+			description: 'The player\'s username; defaults to your linked account, if any',
+			type: ApplicationCommandOptionType.String
+		},
+		{
+			name: 'platform',
+			description: 'The player\'s platform; defaults to Epic',
+			type: ApplicationCommandOptionType.String,
+			choices: PlatformChoices
+		},
+		{
+			name: 'link',
+			description: 'Whether to link this player\'s account with the bot; defaults to false',
+			type: ApplicationCommandOptionType.Boolean
+		}
+	],
+	scope: 'Global',
+	async execute(interaction) {
+		await interaction.deferReply();
+
+		const accountName = interaction.options.getString('player');
+		const accountType = (interaction.options.getString('platform') ?? 'epic') as AccountType;
+
+		let stats: Stats<false>;
+		if (accountName !== null) {
+			try {
+				stats = await fortniteAPI.stats({ name: accountName, accountType });
+			}
+			catch (error) {
+				await handleStatsError(interaction, error);
+				return;
+			}
+		}
+		else {
+			const userResult = getUser(interaction.user.id);
+			if (userResult === null || userResult.epicAccountId === null) {
+				await interaction.editReply('No player username was provided, and you have not linked your account with </link:1032454252024565821>.');
+				return;
+			}
+			stats = await fortniteAPI.stats({ id: userResult.epicAccountId });
+
+		}
+
+		const buffer = await createRankedImage(stats.account, true);
+		await interaction.editReply({ files: [buffer] });
+
+		if (interaction.options.getString('link')) await linkEpicAccount(interaction, stats.account);
+	}
+});
