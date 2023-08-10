9910e2e55d428314570ef67a588fe54672523a70
Squiddleton <mothmain9515@outlook.com>
2023-08-10 09:55:47 -0400

    Populate caches after connecting to mongoose

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,21 @@ import { fetchCosmetics } from './util/fortnite.js';
 import { handleDisconnect } from './util/functions.js';
 import { populateUsers } from './util/users.js';
 
-await fetchCosmetics();
-await populateUsers();
-
 await set('strictQuery', 'throw')
 	.connect(config.mongoPath)
 	.catch(handleDisconnect);
+console.log('Connected to MongoDB');
+
+await fetchCosmetics();
+await populateUsers();
+console.log('Populated caches');
 
 const authenticate = async () => {
 	await epicClient.auth.authenticate(config.epicDeviceAuth.device1);
 	setTimeout(authenticate, 7200000);
 };
 await authenticate();
+console.log('Authenticated with Epic Games');
 
 await client
 	.login(config.token)
