b7746fa0fab95db6f452f9c703c7110f6bad5085
Squiddleton <mothmain9515@outlook.com>
2023-09-22 14:42:28 -0400

    Stop sending messages when leaving guilds

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -1,15 +1,11 @@
 import { ClientEvent } from '@squiddleton/discordjs-util';
 import guildModel from '../models/guilds.js';
 import memberModel from '../models/members.js';
-import { DiscordClient } from '../util/classes.js';
 
 export default new ClientEvent({
 	name: 'guildDelete',
 	async execute(guild) {
 		await guildModel.findByIdAndDelete(guild.id);
 		await memberModel.deleteMany({ guildId: guild.id });
-		const { client } = guild;
-		DiscordClient.assertReadyClient(client);
-		await client.devChannel.send(`${client.user.username} has been removed from ${guild.name}.`);
 	}
 });
