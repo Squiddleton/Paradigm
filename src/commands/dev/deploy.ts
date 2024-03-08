b005d452a7ca43f144c58e4ee0859de039adb527
Squiddleton <mdlegomaster@gmail.com>
2024-03-08 14:31:45 -0500

    Stop editing message upon deployment

--- src/commands/dev/deploy.ts
+++ src/commands/dev/deploy.ts
@@ -1,7 +1,5 @@
 import { SlashCommand } from '@squiddleton/discordjs-util';
-import { ApplicationCommandType, bold, chatInputApplicationCommandMention } from 'discord.js';
 import { DiscordClient } from '../../util/classes.js';
-import { DiscordIds } from '../../util/constants.js';
 
 export default new SlashCommand({
 	name: 'deploy',
@@ -12,11 +10,6 @@ export default new SlashCommand({
 
 		DiscordClient.assertReadyClient(client);
 		await client.deploy();
-		const commands = await client.application.commands.fetch().then(all => all
-			.filter(c => c.type === ApplicationCommandType.ChatInput && c.defaultMemberPermissions === null)
-			.sort((a, b) => a.name.localeCompare(b.name))
-		);
-		await client.getGuildChannel(DiscordIds.ChannelId.UserCommands).messages.edit(DiscordIds.MessageId.CommandList, `${bold(`${client.user} Commands`)}\n\n${commands.map(c => `${chatInputApplicationCommandMention(c.name, c.id)}: ${c.description}`).join('\n')}`);
 
 		await interaction.editReply('Deployed all application commands.');
 	}
