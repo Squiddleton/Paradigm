b4cbaffe2bb88d0679cb1b91b8ac82809dd5967e
Squiddleton <mdlegomaster@gmail.com>
2024-10-02 11:12:11 -0400

    Add tracking players other than command user

--- src/commands/guild/track.ts
+++ src/commands/guild/track.ts
@@ -1,8 +1,11 @@
 import { SlashCommand } from '@squiddleton/discordjs-util';
 import { ApplicationCommandOptionType, chatInputApplicationCommandMention } from 'discord.js';
-import { DiscordIds, RankedTrack } from '../../util/constants.js';
+import { DiscordIds, PlatformChoices, RankedTrack } from '../../util/constants.js';
 import { trackedModes } from '../../util/epic.js';
 import { getUser } from '../../util/users.js';
+import { handleStatsError } from '../../util/fortnite.js';
+import fortniteAPI from '../../clients/fortnite.js';
+import type { AccountType, EpicAccount } from '@squiddleton/fortnite-api';
 
 export default new SlashCommand({
 	name: 'track',
@@ -20,22 +23,52 @@ export default new SlashCommand({
 				{ name: 'Reload (ZB)', value: RankedTrack.Reload1ZB },
 				{ name: 'Rocket Racing', value: RankedTrack.InfernoIslandRacing }
 			]
+		},
+		{
+			name: 'player',
+			description: 'The player\'s username; defaults to your linked account, if any',
+			type: ApplicationCommandOptionType.String
+		},
+		{
+			name: 'platform',
+			description: 'The player\'s platform; defaults to Epic',
+			type: ApplicationCommandOptionType.String,
+			choices: PlatformChoices
 		}
 	],
 	scope: 'Exclusive',
 	async execute(interaction) {
-		const userResult = getUser(interaction.user.id);
-		if (!userResult?.epicAccountId) {
-			await interaction.reply({ content: `You have not linked your account with ${chatInputApplicationCommandMention('link', DiscordIds.CommandId.Link)}.`, ephemeral: true });
-			return;
+		const accountName = interaction.options.getString('player');
+		const accountType = (interaction.options.getString('platform') ?? 'epic') as AccountType;
+
+		let account: EpicAccount;
+		if (accountName !== null) {
+			try {
+				const stats = await fortniteAPI.stats({ name: accountName, accountType });
+				account = stats.account;
+			}
+			catch (error) {
+				await handleStatsError(interaction, error, accountType);
+				return;
+			}
+		}
+		else {
+			const userResult = getUser(interaction.user.id);
+			if (!userResult?.epicAccountId) {
+				await interaction.editReply(`No player username was provided, and you have not linked your account with ${chatInputApplicationCommandMention('link', DiscordIds.CommandId.Link)}.`);
+				return;
+			}
+			account = { name: interaction.user.displayName, id: userResult.epicAccountId };
 		}
 
+		const accountId = account.id;
 		const track = interaction.options.getString('mode', true) as RankedTrack.C5S4BR | RankedTrack.C5S4ZB | RankedTrack.InfernoIslandRacing | RankedTrack.Reload1BR | RankedTrack.Reload1ZB;
 
-		if (!trackedModes.has(userResult.epicAccountId)) trackedModes.set(userResult.epicAccountId, { displayUsername: interaction.user.displayName, trackedModes: [] });
-		const trackedUser = trackedModes.get(userResult.epicAccountId);
+		if (!trackedModes.has(accountId)) trackedModes.set(accountId, { displayUsername: account.name, trackedModes: [] });
+		const trackedUser = trackedModes.get(accountId);
 		if (trackedUser === undefined) {
 			await interaction.reply('An error occurred while getting the current mode to track.');
+			console.error(`No tracked user found for account ${accountId}:`, trackedModes);
 			return;
 		}
 
@@ -50,11 +83,11 @@ export default new SlashCommand({
 
 		if (existingTrack === undefined) {
 			trackedUser.trackedModes.push({ trackguid: track, displayName: trackDisplayName });
-			await interaction.reply(`Started tracking your progress in ${trackDisplayName}!`);
+			await interaction.reply(`Started tracking progress for ${account.name} in ${trackDisplayName}!`);
 		}
 		else {
 			trackedUser.trackedModes.splice(trackedUser.trackedModes.indexOf(existingTrack), 1);
-			await interaction.reply(`Stopped tracking your progress in ${trackDisplayName}.`);
+			await interaction.reply(`Stopped tracking progress for ${account.name} in ${trackDisplayName}.`);
 		}
 	}
 });
