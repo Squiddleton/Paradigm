5ff8915314ce5ab94e9afdcd9643b1cb58fb8381
Squiddleton <mothmain9515@outlook.com>
2024-12-13 17:36:13 -0500

    Replace default game client with Android

--- src/clients/epic.ts
+++ src/clients/epic.ts
@@ -1,6 +1,6 @@
 import { EpicClient } from '@squiddleton/epic';
 import { ChapterLengths } from '../util/constants.js';
 
-const epicClient = new EpicClient({ seasonsLength: ChapterLengths.reduce((p, c) => p + c, 0) });
+const epicClient = new EpicClient({ seasonsLength: ChapterLengths.reduce((p, c) => p + c, 0), gameClient: 'M2Y2OWU1NmM3NjQ5NDkyYzhjYzI5ZjFhZjA4YThhMTI6YjUxZWU5Y2IxMjIzNGY1MGE2OWVmYTY3ZWY1MzgxMmU=' });
 
 export default epicClient;
